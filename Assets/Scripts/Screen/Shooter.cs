2949b78e68455bc045a48b605459b8075af01f7e
Dragos <cam01884@myport.ac.uk>
2019-01-10 22:31:13 +0000

    ssh added

--- Assets/Scripts/Screen/Shooter.cs
+++ Assets/Scripts/Screen/Shooter.cs
@@ -11,8 +11,14 @@ public enum ShooterState {
 }
 public class Shooter : MonoBehaviour
 {
-  private ShooterState state;
-  public float health { get; set; }
+  public float health { get; set; } = 50.0f;
+  private bool readyToDie = false;
+  private TargetInput targetInput = new TargetInput();
+  private TargetState state = ShooterState.Start;
+  private int secondsStopped = 0;
+  private Vector3 startPosition = new Vector3();
+  private Vector3 stopPosition = new Vector3();
+  private DateTime stopTime = new DateTime();
 
   void Update() {
     switch(state) {
@@ -31,8 +37,45 @@ public class Shooter : MonoBehaviour
     }
   }
 
-  OnShootAction() {
+  void OnShootAction() {
 
   }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if(readyToDie)
+    {
+      if(other.gameObject.layer == LayerMask.NameToLayer("Ship"))
+      {
+        health
+      }      
+    }
+  }
+
+  private void OnTriggerExit(Collider other)
+  {
+    readyToDie = true;
+    
+    if(gameObject.GetComponent<Rigidbody>() as Rigidbody != null)
+    {
+      var rb = gameObject.AddComponent<Rigidbody>() as Rigidbody;
+      if(rb != null)
+      {
+        rb.useGravity = false;  
+      }    
+    }    
+  }
+
+  void OnInitialise(ShooterInput input)
+  {
+    if(input != null)
+    {
+      targetInput = input;
+    }
+
+    startPosition = targetInput.StartPosition + new Vector3(targetInput.Offset.x, 0.0f, targetInput.Offset.y);
+    transform.position = startPosition;
+    stopPosition = targetInput.StopPosition + new Vector3(targetInput.Offset.x, 0.0f, targetInput.Offset.y);
+  } 
 }
 
